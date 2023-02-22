6489edd5868809bcb194450c52be042324836918
ankitbhurji <bhurji204@gmail.com>
2023-02-23 01:42:59 +0530

    add portfolio

---
+++ index.js
@@ -0,0 +1,84 @@
+function projectOpen(target_id) {
+    switch(target_id){
+        case "mern-1":
+            window.open("https://dreamy-store-app-frontend.onrender.com/")
+            break;
+        case 'mern-2':
+            window.open("https://centralized-file-manager-app.onrender.com/")
+            break;
+        case 'mern-3':
+            window.open("https://ma-frontend.onrender.com/")
+            break
+        case 'mern-4':
+            window.open("https://blog-by-nodeandmongodb.onrender.com/")
+            break
+        case 'mern-5':
+            window.open("https://todo-by-nodeandmongodb.onrender.com/")
+            break
+
+        case 'react-1':
+            window.open("https://daily-note-tracker.onrender.com/")
+            break
+        case 'react-2':
+            window.open("https://bill-generator-733s.onrender.com/")
+            break
+        case 'react-3':
+            window.open("https://password-generator-nstf.onrender.com/")
+            break
+        case 'react-4':
+            window.open("https://advice-y4l3.onrender.com/")
+            break
+        case 'react-5':
+            window.open("https://mastercard.onrender.com/")
+            break
+        case 'react-6':
+            window.open("https://ankitbhurji.github.io/React_todo/")
+            break
+        case 'react-7':
+            window.open("https://ankitbhurji.github.io/React_Weather/")
+            break
+        case 'react-8':
+            window.open("https://keeper-app-by-react.onrender.com/")
+            break
+
+        case 'js-1':
+            window.open("https://ankitbhurji.github.io/Drum/")
+            break
+        case 'js-2':
+            window.open("https://ankitbhurji.github.io/DiceeGame/")
+            break
+        case 'js-3':
+            window.open("https://ankitbhurji.github.io/Calculator-By-Js/")
+            break
+        case 'js-4':
+            window.open("https://ankitbhurji.github.io/StopWatch-By-Js/")
+            break
+        case 'js-5':
+            window.open("https://ankitbhurji.github.io/Clock-By-Js/")
+            break
+        case 'js-6':
+            window.open("https://ankitbhurji.github.io/ColorMode_By_js/")
+            break
+        case 'js-7':
+            window.open("https://ankitbhurji.github.io/ToDo-By-Js/")
+            break
+        case 'js-8':
+            window.open("https://ankitbhurji.github.io/TempConverter-By-Js/")
+            break
+        case 'js-9':
+            window.open("https://ankitbhurji.github.io/WordCounter-By-Js/")
+            break
+        case 'js-10':
+            window.open("https://ankitbhurji.github.io/Simon-Game/")
+            break
+        case 'js-11':
+            break
+            window.open("https://ankitbhurji.github.io/FormValidation-By-Js/")
+        default:
+            console.log("url does not exist")
+
+            
+    }
+    // window.open("https://www.w3schools.com");
+    // console.log(target_id)
+}
